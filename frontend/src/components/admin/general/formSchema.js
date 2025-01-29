6e90b865ecb508d7008e933a5456e7355779c638
Abhinav Raut <abhinavrautcs@gmail.com>
2025-01-30 01:50:33 +0530

    improvements and fixes to zod form schemas

--- frontend/src/components/admin/general/formSchema.js
+++ frontend/src/components/admin/general/formSchema.js
@@ -35,7 +35,7 @@ export const formSchema = z.object({
     .min(1, {
       message: 'Max upload file size must be at least 1 MB.'
     })
-    .max(1024, {
+    .max(128, {
       message: 'Max upload file size cannot exceed 128 MB.'
     }),
   allowed_file_upload_extensions: z.array(z.string()).nullable().default([]).optional()
--- frontend/src/components/admin/sla/formSchema.js
+++ frontend/src/components/admin/sla/formSchema.js
@@ -4,19 +4,21 @@ import { isGoHourMinuteDuration } from '@/utils/strings'
 export const formSchema = z.object({
     name: z
         .string()
+        .min(1, { message: 'Name is required' })
         .max(255, {
             message: 'Name must be at most 255 characters.'
         }),
     description: z
         .string()
+        .min(1, { message: 'Description is required' })
         .max(255, {
             message: 'Description must be at most 255 characters.'
         }),
-    first_response_time: z.string().optional().refine(isGoHourMinuteDuration, {
+    first_response_time: z.string().refine(isGoHourMinuteDuration, {
         message:
             'Invalid duration format. Should be a number followed by h (hours), m (minutes).'
     }),
-    resolution_time: z.string().optional().refine(isGoHourMinuteDuration, {
+    resolution_time: z.string().refine(isGoHourMinuteDuration, {
         message:
             'Invalid duration format. Should be a number followed by h (hours), m (minutes).'
     }),

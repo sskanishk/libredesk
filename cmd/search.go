3365dec441992fc93c85764f8d0b7316b09c4fde
Abhinav Raut <abhinavrautcs@gmail.com>
2025-02-20 03:36:22 +0530

    fix: disallow search queries with less than 3 characters

--- cmd/search.go
+++ cmd/search.go
@@ -1,6 +1,13 @@
 package main
 
-import "github.com/zerodha/fastglue"
+import (
+	"github.com/abhinavxd/libredesk/internal/envelope"
+	"github.com/zerodha/fastglue"
+)
+
+const (
+	minSearchQueryLength = 3
+)
 
 // handleSearchConversations searches conversations based on the query.
 func handleSearchConversations(r *fastglue.Request) error {
@@ -8,6 +15,11 @@ func handleSearchConversations(r *fastglue.Request) error {
 		app = r.Context.(*App)
 		q   = string(r.RequestCtx.QueryArgs().Peek("query"))
 	)
+
+	if len(q) < minSearchQueryLength {
+		return sendErrorEnvelope(r, envelope.NewError(envelope.InputError, "Query length should be at least 3 characters", nil))
+	}
+
 	conversations, err := app.search.Conversations(q)
 	if err != nil {
 		return sendErrorEnvelope(r, err)
@@ -21,6 +33,11 @@ func handleSearchMessages(r *fastglue.Request) error {
 		app = r.Context.(*App)
 		q   = string(r.RequestCtx.QueryArgs().Peek("query"))
 	)
+
+	if len(q) < minSearchQueryLength {
+		return sendErrorEnvelope(r, envelope.NewError(envelope.InputError, "Query length should be at least 3 characters", nil))
+	}
+
 	messages, err := app.search.Messages(q)
 	if err != nil {
 		return sendErrorEnvelope(r, err)

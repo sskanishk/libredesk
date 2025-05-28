38a5b25b1f093764002646ed7a1885823e5b4dcd
Abhinav Raut <abhinavrautcs@gmail.com>
2025-05-29 01:11:14 +0530

    remove search icon from search header

--- frontend/src/features/search/SearchHeader.vue
+++ frontend/src/features/search/SearchHeader.vue
@@ -1,16 +1,12 @@
 <template>
   <div>
-    <div class="flex items-center space-x-4 px-2 h-12">
+    <div class="flex items-center px-2 h-12">
       <SidebarTrigger class="cursor-pointer" />
-      <div class="flex-1 flex items-center">
-        <Search class="w-5 h-5" />
-        <Separator orientation="vertical" />
-        <Input
-          v-model="model"
-          :placeholder="t('globals.terms.search')"
-          class="w-full border-none shadow-none focus:ring-0 focus:ring-offset-0"
-        />
-      </div>
+      <Input
+        v-model="model"
+        :placeholder="t('globals.terms.search')"
+        class="w-full border-none shadow-none focus:ring-0 focus:ring-offset-0"
+      />
     </div>
     <Separator />
   </div>
@@ -20,7 +16,6 @@
 import { Separator } from '@/components/ui/separator'
 import { Input } from '@/components/ui/input'
 import { SidebarTrigger } from '@/components/ui/sidebar'
-import { Search } from 'lucide-vue-next'
 import { useI18n } from 'vue-i18n'
 const model = defineModel({
   type: String,

331c84fa56368cec592fa2c91c27b3095bec9f57
Abhinav Raut <abhinavrautcs@gmail.com>
2025-05-31 23:40:42 +0530

    use dot loader to use tailwind animations

--- frontend/src/components/ui/loader/DotLoader.vue
+++ frontend/src/components/ui/loader/DotLoader.vue
@@ -1,7 +1,11 @@
 <template>
-  <span class="dot-loader">
-    <span class="dot"></span>
-    <span class="dot"></span>
-    <span class="dot"></span>
+  <span class="inline-flex items-center">
+    <span class="w-1 h-1 rounded-full bg-current mx-0.5 animate-dot-flashing"></span>
+    <span
+      class="w-1 h-1 rounded-full bg-current mx-0.5 animate-dot-flashing [animation-delay:0.2s]"
+    ></span>
+    <span
+      class="w-1 h-1 rounded-full bg-current mx-0.5 animate-dot-flashing [animation-delay:0.4s]"
+    ></span>
   </span>
 </template>

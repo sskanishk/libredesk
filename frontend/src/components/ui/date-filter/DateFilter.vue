fea7eef658be75b3a956692fb983f832e1f49ad4
Abhinav Raut <abhinavrautcs@gmail.com>
2025-06-07 22:37:07 +0530

    dynamic translations in date filter component

--- frontend/src/components/ui/date-filter/DateFilter.vue
+++ frontend/src/components/ui/date-filter/DateFilter.vue
@@ -12,12 +12,53 @@
       </SelectTrigger>
       <SelectContent class="text-xs">
         <SelectItem value="0">{{ $t('globals.terms.today') }}</SelectItem>
-        <SelectItem value="1">{{ $t('filters.last_1_day') }}</SelectItem>
-        <SelectItem value="2">{{ $t('filters.last_2_days') }}</SelectItem>
-        <SelectItem value="7">{{ $t('filters.last_7_days') }}</SelectItem>
-        <SelectItem value="30">{{ $t('filters.last_30_days') }}</SelectItem>
-        <SelectItem value="90">{{ $t('filters.last_90_days') }}</SelectItem>
-        <SelectItem value="custom">{{ $t('filters.custom_days') }}</SelectItem>
+        <SelectItem value="1">
+          {{
+            $t('globals.messages.lastNItems', {
+              n: 1,
+              name: t('globals.terms.day', 1).toLowerCase()
+            })
+          }}
+        </SelectItem>
+        <SelectItem value="2">
+          {{
+            $t('globals.messages.lastNItems', {
+              n: 2,
+              name: t('globals.terms.day', 2).toLowerCase()
+            })
+          }}
+        </SelectItem>
+        <SelectItem value="7">
+          {{
+            $t('globals.messages.lastNItems', {
+              n: 7,
+              name: t('globals.terms.day', 2).toLowerCase()
+            })
+          }}
+        </SelectItem>
+        <SelectItem value="30">
+          {{
+            $t('globals.messages.lastNItems', {
+              n: 30,
+              name: t('globals.terms.day', 2).toLowerCase()
+            })
+          }}
+        </SelectItem>
+        <SelectItem value="90">
+          {{
+            $t('globals.messages.lastNItems', {
+              n: 90,
+              name: t('globals.terms.day', 2).toLowerCase()
+            })
+          }}
+        </SelectItem>
+        <SelectItem value="custom">
+          {{
+            $t('globals.messages.custom', {
+              name: t('globals.terms.day', 2).toLowerCase()
+            })
+          }}
+        </SelectItem>
       </SelectContent>
     </Select>
     <div v-if="selectedDays === 'custom'" class="flex items-center gap-2">
@@ -26,14 +67,10 @@
         type="number"
         min="1"
         max="365"
-        :placeholder="$t('filters.days_placeholder')"
-        class="w-20"
+        class="w-20 h-8"
         @blur="handleCustomDaysChange"
         @keyup.enter="handleCustomDaysChange"
       />
-      <span class="text-xs text-muted-foreground">{{
-        $t('globals.terms.day', 2).toLowerCase()
-      }}</span>
     </div>
   </div>
 </template>
